8b5f82a16d73f50193aac3be02c769f77c5aeef9
Sabir Kirpal <skirpal2000@gmail.com>
2022-01-06 14:53:11 -0800

    Finished 3.2

--- src/main/java/stackqueue/_03_02_MinStack.java
+++ src/main/java/stackqueue/_03_02_MinStack.java
@@ -1,20 +1,105 @@
 package stackqueue;
 
-/**
- * How would you design a stack which, in addition to push and pop, has a function min which returns the minimum element? Push, pop and min should all operate in 0(1) time.
- */
-class _03_02_MinStack {
+import java.util.EmptyStackException;
 
+/**
+ * How would you design a stack which, in addition to push and pop, has a
+ * function min which returns the minimum element? Push, pop and min should all
+ * operate in 0(1) time.
+ */
+public class _03_02_MinStack<T> {
+
+    private static class StackNode<T> {
+        private int data;
+        private StackNode<Integer> next;
+
+        public StackNode(int data) {
+            this.data = data;
+        }
+
+    }
+
+    private StackNode<Integer> top;
+    private StackNode<Integer> min = new StackNode<Integer>(Integer.MAX_VALUE);
 
     void push(int val) {
-        throw new UnsupportedOperationException();
+        StackNode<Integer> newEntry = new StackNode<Integer>(val);
+        newEntry.next = top;
+        top = newEntry;
+
+        if (val < min.data) {
+            min = top;
+        }
+
+    }
+
+    void printStack() {
+        System.out.print("Stack = [ ");
+
+        StackNode<Integer> t = top;
+
+        while (t != null) {
+            System.out.print(t.data + ", ");
+            t = t.next;
+        }
+
+        System.out.println(" ]");
     }
 
     int pop() {
-        throw new UnsupportedOperationException();
+        if (top == null) {
+            throw new EmptyStackException();
+        }
+
+        StackNode<Integer> t = top;
+
+        if (min == top) {
+            int newMin = Integer.MAX_VALUE;
+            StackNode<Integer> x = top.next;
+            if (x == null) {
+                min = null;
+            }
+            StackNode<Integer> y = null;
+            while (x != null) {
+
+                if (x.data < newMin) {
+                    System.out.println("Hit once");
+                    y = x;
+                    newMin = y.data;
+                }
+                x = x.next;
+            }
+
+            if (y == null) {
+                min = x;
+            } else {
+                min = y;
+            }
+
+        }
+
+        top = top.next;
+
+        return t.data;
+
     }
 
     int min() {
-        throw new UnsupportedOperationException();
+        if (min != null) {
+            System.out.println("Min = " + min.data);
+            return min.data;
+        } else {
+            System.out.println("Min = STACK EMPTY");
+            return Integer.MIN_VALUE;
+        }
+    }
+
+    public static void main(String[] args) {
+        _03_02_MinStack<Integer> s = new _03_02_MinStack<Integer>();
+        s.pop();
     }
 }
+
+// either maintain priority queue as obj attribute
+// or you can search through stack eveerytime you pop
+// or you can search through stack everytime you ask for min

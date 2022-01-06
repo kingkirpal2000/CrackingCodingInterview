cfd040209334d71559b8fea88268537588960663
Sabir Kirpal <skirpal2000@gmail.com>
2022-01-06 15:45:39 -0800

    Found out 3.2 was wrong, moving on to 3.3

--- src/main/java/stackqueue/_03_02_MinStack.java
+++ src/main/java/stackqueue/_03_02_MinStack.java
@@ -7,43 +7,25 @@ import java.util.EmptyStackException;
  * function min which returns the minimum element? Push, pop and min should all
  * operate in 0(1) time.
  */
-public class _03_02_MinStack<T> {
+class _03_02_MinStack<T> {
+    private static class stackNode<T> {
+        private int value;
+        private int min;
+        private stackNode<T> next;
 
-    private static class StackNode<T> {
-        private int data;
-        private StackNode<Integer> next;
-
-        public StackNode(int data) {
-            this.data = data;
+        public stackNode(int value, int min) {
+            this.value = value;
+            this.min = min;
         }
-
     }
 
-    private StackNode<Integer> top;
-    private StackNode<Integer> min = new StackNode<Integer>(Integer.MAX_VALUE);
+    private stackNode<Integer> top;
 
     void push(int val) {
-        StackNode<Integer> newEntry = new StackNode<Integer>(val);
-        newEntry.next = top;
-        top = newEntry;
-
-        if (val < min.data) {
-            min = top;
-        }
-
-    }
-
-    void printStack() {
-        System.out.print("Stack = [ ");
-
-        StackNode<Integer> t = top;
-
-        while (t != null) {
-            System.out.print(t.data + ", ");
-            t = t.next;
-        }
-
-        System.out.println(" ]");
+        int newMin = Math.min(val, min());
+        stackNode<Integer> t = new stackNode<Integer>(val, newMin);
+        t.next = top;
+        top = t;
     }
 
     int pop() {
@@ -51,55 +33,42 @@ public class _03_02_MinStack<T> {
             throw new EmptyStackException();
         }
 
-        StackNode<Integer> t = top;
+        stackNode<Integer> t = top;
+        top = top.next;
+        return t.value;
+    }
 
-        if (min == top) {
-            int newMin = Integer.MAX_VALUE;
-            StackNode<Integer> x = top.next;
-            if (x == null) {
-                min = null;
-            }
-            StackNode<Integer> y = null;
-            while (x != null) {
+    void printStack() {
+        System.out.print("Stack = [ ");
 
-                if (x.data < newMin) {
-                    System.out.println("Hit once");
-                    y = x;
-                    newMin = y.data;
-                }
-                x = x.next;
-            }
+        stackNode<Integer> t = top;
 
-            if (y == null) {
-                min = x;
-            } else {
-                min = y;
-            }
-
+        while (t != null) {
+            System.out.print(t.value + ", ");
+            t = t.next;
         }
 
-        top = top.next;
-
-        return t.data;
-
+        System.out.println(" ]");
     }
 
     int min() {
-        if (min != null) {
-            System.out.println("Min = " + min.data);
-            return min.data;
+        stackNode<Integer> t = top;
+        if (t != null) {
+            return t.min;
         } else {
-            System.out.println("Min = STACK EMPTY");
-            return Integer.MIN_VALUE;
+            return Integer.MAX_VALUE;
         }
     }
 
     public static void main(String[] args) {
-        _03_02_MinStack<Integer> s = new _03_02_MinStack<Integer>();
-        s.pop();
+        _03_02_MinStack<Integer> obj = new _03_02_MinStack<Integer>();
+        obj.push(5);
+        obj.printStack();
+        obj.push(4);
+        obj.push(6);
+        obj.pop();
+        System.out.println(obj.min());
+        obj.pop();
+        System.out.println(obj.min());
     }
 }
-
-// either maintain priority queue as obj attribute
-// or you can search through stack eveerytime you pop
-// or you can search through stack everytime you ask for min

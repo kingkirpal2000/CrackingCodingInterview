6f32ee946e02cce7312996009950b30633bedfe1
Sabir Kirpal <skirpal2000@gmail.com>
2022-01-18 23:26:36 -0800

    Ez pz

--- src/main/java/treegraph/_04_01_RouteBetweenNodes.java
+++ src/main/java/treegraph/_04_01_RouteBetweenNodes.java
@@ -1,5 +1,12 @@
 package treegraph;
 
+import java.util.Queue;
+
+import javax.management.QueryEval;
+
+import datastructures.LinkedList;
+import java.util.*;
+
 /**
  * Given a directed graph, design an algorithm to find out whether
  * there is a route between two nodes.
@@ -13,7 +20,41 @@ class _04_01_RouteBetweenNodes {
     }
 
     boolean hasRoute(int source, int target) {
-        throw new UnsupportedOperationException();
+        ArrayList<Integer> queue = new ArrayList<Integer>();
+        queue.add(source);
+
+        ArrayList<Integer> visited = new ArrayList<Integer>();
+        visited.add(source);
+
+        while (!queue.isEmpty()) {
+            int x = queue.remove(0);
+            Iterable<Integer> neighbors = digraph.adjacent(x);
+            System.out.println(neighbors);
+            for (int neighbor : neighbors) {
+                if (neighbor == target) {
+                    return true;
+                }
+                if (!visited.contains(neighbor)) {
+                    queue.add(neighbor);
+                    visited.add(neighbor);
+                }
+            }
+
+        }
+
+        return false;
+
+    }
+
+    public static void main(String[] args) {
+        Digraph digraph = new Digraph(5);
+        digraph.addEdge(0, 1);
+        digraph.addEdge(0, 2);
+        digraph.addEdge(2, 3);
+        digraph.addEdge(2, 4);
+        _04_01_RouteBetweenNodes obj = new _04_01_RouteBetweenNodes(digraph);
+        System.out.println(obj.hasRoute(0, 5));
+
     }
 
 }
